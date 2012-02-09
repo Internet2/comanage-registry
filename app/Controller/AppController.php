dc6329f13aa8b83eb25db9c2e3bd0235334997d8
Benn Oshrin <boshrin@noreply.users.github.com>
2012-02-09 01:31:16 +0000

    Better checking for home.ctp permissions

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -144,9 +144,12 @@ class AppController extends Controller {
       $this->Security->csrfCheck = false;
     }
     
-    if($this->name == 'Pages' && (!$this->Session->check('Auth.User'))) {
+    if($this->name == 'Pages'
+       && $this->request->params['pass'][0] == 'home'
+       && (!$this->Session->check('Auth.User'))) {
       // Allow the front page to render without authentication. If there is an
       // authenticated user, we want Auth to run to set up authorizations.
+      
       $this->Auth->allow('*');
     }
   }
