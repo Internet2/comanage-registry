6ec26625a52553225934ca44f3ed31fd55bdddff
Scott Koranda <scott.koranda@ligo.org>
2015-01-21 15:53:14 -0600

    Shib Embedded Discovery Service Plugin (CO-773)

---
+++ app/Plugin/ShibbolethEmbeddedDiscoveryService/Model/ShibbolethEmbeddedDiscoveryService.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * COmanage Registry Shibboleth Embedded Discovery Service Model
+ *
+ * Copyright (C) 2015 University Corporation for Advanced Internet Development, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * @copyright     Copyright (C) 2015 University Corporation for Advanced Internet Development, Inc.
+ * @link          http://www.internet2.edu/comanage COmanage Project
+ * @package       registry-plugin
+ * @since         COmanage Registry v1.0
+ * @license       Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * @version       $Id$
+ */
+
+class ShibbolethEmbeddedDiscoveryService extends AppModel {
+  // Required by COmanage Plugins
+  public $cmPluginType = "other";
+
+  /**
+   * Expose menu items.
+   * 
+   * @ since COmanage Registry v0.9.2
+   * @ return Array with menu location type as key and array of labels, controllers, actions as values.
+   */
+  public function cmPluginMenus() {
+  	return array();
+  }
+}
