a2ae611dd6f33b6173f1bcf692c98a1cc7c28e69
Benn Oshrin <boshrin@noreply.users.github.com>
2015-02-28 17:43:53 -0500

    Additional commit for CO-797

--- app/Model/CoNotification.php
+++ app/Model/CoNotification.php
@@ -2,7 +2,7 @@
 /**
  * COmanage Registry CO Notification Model
  *
- * Copyright (C) 2014 University Corporation for Advanced Internet Development, Inc.
+ * Copyright (C) 2014-15 University Corporation for Advanced Internet Development, Inc.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -14,7 +14,7 @@
  * KIND, either express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  *
- * @copyright     Copyright (C) 2014 University Corporation for Advanced Internet Development, Inc.
+ * @copyright     Copyright (C) 2014-15 University Corporation for Advanced Internet Development, Inc.
  * @link          http://www.internet2.edu/comanage COmanage Project
  * @package       registry
  * @since         COmanage Registry v0.8.4
@@ -610,7 +610,9 @@ class CoNotification extends AppModel {
     }
     
     $n['CoNotification']['action'] = $action;
-    $n['CoNotification']['comment'] = $comment;
+    
+    $limit = $this->validate['comment']['rule'][1];
+    $n['CoNotification']['comment'] = substr($comment, 0, $limit);
     
     $csource = array();
     
