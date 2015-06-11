204c512d3745216ff4e4bd20b0d1004f848059aa
Scott Koranda <scott.koranda@ligo.org>
2015-06-11 12:55:14 -0500

    Unit tests and fixture for Co model (CO-97)

---
+++ app/Test/Case/Model/CoTest.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * COmanage Registry CO Model Test
+ *
+ * Copyright (C) 2015 University Corporation for Advanced Internet Development, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * @copyright     Copyright (C) 2015 University Corporation for Advanced Internet Development, Inc.
+ * @link          http://www.internet2.edu/comanage COmanage Project
+ * @package       registry
+ * @since         COmanage Registry v0.9.4
+ * @license       Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * @version       $Id$
+ */
+
+App::uses('Co', 'Model');
+
+class CoTest extends CakeTestCase {
+  public $fixtures = array('app.co');
+
+  public function setUp() {
+    parent::setUp();
+    $this->Co = ClassRegistry::init('Co');
+  }
+
+  /**
+   * Test afterSave method of class Co.
+   *
+   * We test the afterSave method by creating and saving a new CO.
+   * To check that afterSave is correctly invoked we check that
+   * for the new CO the associated model CoExtendedType is created. 
+   * We do not check the actual contents of the CoExtendedType instance 
+   * here and leave that to the unit test for the CoExtendedType model.
+   *
+   * @since  COmanage Registry v0.9.4
+   */
+  public function testAfterSave() {
+    // Data for a new CO.
+    $data = array();
+    $data['Co']['name'] = 'A new CO';
+    $data['Co']['description'] = 'Description for A new CO';
+    $data['Co']['status'] = 'A';
+
+    // Save the data. The method afterSave() should be automatically invoked.
+    $this->Co->save($data);
+
+    // Query to find the Co just created and use containable behavior
+    // to include CoExtendedType if linked to the Co.
+    $params = array();
+    $params['conditions'] = array();
+    $params['conditions']['Co.name'] = 'A new CO';
+    $params['contain'] = array();
+    $params['contain'][] = 'CoExtendedType';
+    $co = $this->Co->find('first', $params);
+
+    // We just test for presence of the Co and CoExtendedType keys.
+    $keys = array_keys($co);
+    ksort($keys);
+    $result = $keys;
+
+    $expected = array();
+    $expected[] = 'Co';
+    $expected[] = 'CoExtendedType';
+
+    $this->assertEquals($expected, $result);
+  }
+
+  /**
+   * Test setup method of class Co.
+   *
+   * We test the setup method by checking that the associated model
+   * CoExtendedType is created. We do not check the actual contents
+   * of the CoExtendedType instance here and leave that to the
+   * unit test for the CoExtendedType model.
+   *
+   * @since  COmanage Registry v0.9.4
+   */
+  public function testSetup() {
+
+    // Invoke the setup() method for Co with id 2 from the fixture.
+    $this->Co->setup(2);
+
+    // Query to find the Co with id 2 and use containable behavior
+    // to include CoExtendedType if linked to the Co.
+    $params = array();
+    $params['conditions'] = array();
+    $params['conditions']['Co.id'] = 2;
+    $params['contain'] = array();
+    $params['contain'][] = 'CoExtendedType';
+    $co = $this->Co->find('first', $params);
+
+    // We just test for presence of the Co and CoExtendedType keys.
+    $keys = array_keys($co);
+    ksort($keys);
+    $result = $keys;
+
+    $expected = array();
+    $expected[] = 'Co';
+    $expected[] = 'CoExtendedType';
+
+    $this->assertEquals($expected, $result);
+  }
+}
---
+++ app/Test/Fixture/CoFixture.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * COmanage Registry CO Fixture
+ *
+ * Copyright (C) 2015 University Corporation for Advanced Internet Development, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * @copyright     Copyright (C) 2015 University Corporation for Advanced Internet Development, Inc.
+ * @link          http://www.internet2.edu/comanage COmanage Project
+ * @package       registry
+ * @since         COmanage Registry v0.9.4
+ * @license       Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * @version       $Id$
+ */
+
+class CoFixture extends CakeTestFixture {
+    // Import schema for the model from the default database.
+    // The fixture data itself will be written to test and
+    // not default.
+    public $import = array('model' => 'Co', 'connection' => 'default');
+
+    public function init() {
+
+      $records = array();
+
+      // Mimic the internal CO always created during deployment.
+      $arecord = array();
+      $arecord['id']          = 1;
+      $arecord['name']        = 'COmanage';
+      $arecord['description'] = 'COmanage Registry Internal CO';
+      $arecord['status']      = SuspendableStatusEnum::Active;
+      $arecord['created']     = date('Y-m-d H:i:s');
+      $arecord['modified']    = date('Y-m-d H:i:s');
+      $records[] = $arecord;
+
+      // A second CO.
+      $arecord = array();
+      $arecord['id']          = 2;
+      $arecord['name']        = 'Test CO 1';
+      $arecord['description'] = 'Description for Test CO 1';
+      $arecord['status']      = SuspendableStatusEnum::Active;
+      $arecord['created']     = date('Y-m-d H:i:s');
+      $arecord['modified']    = date('Y-m-d H:i:s');
+      $records[] = $arecord;
+
+      $this->records = $records;
+
+      parent::init();
+    }
+}
