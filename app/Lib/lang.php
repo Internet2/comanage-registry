a72e6d6052d2244e0f5e9a6a2492c220ffda24cc
Benn Oshrin <boshrin@users.noreply.github.com>
2016-07-15 20:48:14 -0400

    Fix handling of provisioning data during petition provision step (CO-1272)

--- app/Lib/lang.php
+++ app/Lib/lang.php
@@ -944,6 +944,8 @@ original notification at
   'fd.ia.permitted.desc' => 'When substituting parameters in a format, only permit these characters to be used',
   'fd.ia.type.email' => 'Email Type',
   'fd.ia.type.email.desc' => 'For Identifier Assignments applied to Type <i>Mail</i>, an Email Address (instead of an Identifier) will be created with this type (if not blank)',
+  // fd.id.seq should be used only for database internal column IDs
+  'fd.id.seq' =>      'ID',
   // The next set must be named fd.model.validation-field
   'fd.identifier.identifier' => 'Identifier',
   'fd.identifier.login' => 'Login',
