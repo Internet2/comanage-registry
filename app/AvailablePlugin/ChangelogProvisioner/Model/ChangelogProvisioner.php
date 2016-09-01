e42057093e9998d4ab011bcf94dea1131ac19b5a
Benn Oshrin <boshrin@users.noreply.github.com>
2016-08-31 22:41:05 -0400

    Add new AvailablePlugin directory (CO-1316)

--- app/AvailablePlugin/ChangelogProvisioner/Model/ChangelogProvisioner.php
+++ app/AvailablePlugin/ChangelogProvisioner/Model/ChangelogProvisioner.php
@@ -2,7 +2,7 @@
 /**
  * COmanage Registry Changelog Provisioner Model
  *
- * Copyright (C) 2013 University Corporation for Advanced Internet Development, Inc.
+ * Copyright (C) 2013-16 University Corporation for Advanced Internet Development, Inc.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -14,7 +14,7 @@
  * KIND, either express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  *
- * @copyright     Copyright (C) 2013 University Corporation for Advanced Internet Development, Inc.
+ * @copyright     Copyright (C) 2013-16 University Corporation for Advanced Internet Development, Inc.
  * @link          http://www.internet2.edu/comanage COmanage Project
  * @package       registry-plugin
  * @since         COmanage Registry v0.8
@@ -24,7 +24,6 @@
 
 class ChangelogProvisioner extends AppModel {
   // Required by COmanage Plugins
-  // To enable this plugin, change the plugin type to "provisioner"
   public $cmPluginType = "provisioner";
 
   // Document foreign keys
