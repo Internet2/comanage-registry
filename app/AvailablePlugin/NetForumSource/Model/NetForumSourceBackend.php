d59fbf5fdb478116c852e019bdfea78f6546acab
Benn Oshrin <boshrin@users.noreply.github.com>
2016-12-21 11:40:13 -0500

    Fix NetForumSource status check (NOJIRA)

--- app/AvailablePlugin/NetForumSource/Model/NetForumSourceBackend.php
+++ app/AvailablePlugin/NetForumSource/Model/NetForumSourceBackend.php
@@ -236,7 +236,7 @@ class NetForumSourceBackend extends OrgIdentitySourceBackend {
       }
 
       $results = $this->makeNetForumQuery('Email', $searchEmail);
-
+      
       if($results) {
         foreach($results->Result as $r) {
           // Pull the full record returned by customer key in case we're
@@ -244,7 +244,7 @@ class NetForumSourceBackend extends OrgIdentitySourceBackend {
           $r2 = $this->makeNetForumQuery('Key', $r->cst_key);
           
           // There should be only one response per key
-          if($r2 && (string)$r2->MemberStatus == 'Active') {
+          if($r2 && (string)$r2->Result->MemberStatus == 'Active') {
             // Use the customer key as the unique ID
             $ret[ (string)$r->cst_key ] = $this->resultToOrgIdentity($r2->Result[0]);
           }
