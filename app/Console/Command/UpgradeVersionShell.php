ebc4ddd7dd9666388eaa54150f5ccd006d70ab67
Benn Oshrin <boshrin@users.noreply.github.com>
2017-02-08 22:23:28 -0500

    Add v1.0.6 to UpgradeVersionShell

--- app/Console/Command/UpgradeVersionShell.php
+++ app/Console/Command/UpgradeVersionShell.php
@@ -61,6 +61,7 @@ class UpgradeVersionShell extends AppShell {
     "1.0.3" => array('block' => false),
     "1.0.4" => array('block' => false),
     "1.0.5" => array('block' => false, 'post' => 'post105'),
+    "1.0.6" => array('block' => false),
     "1.1.0" => array('block' => false, 'post' => 'post110')
   );
   
