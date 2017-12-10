760454897919d61de145d2ad173b6d3b6472c5a0
Benn Oshrin <boshrin@users.noreply.github.com>
2017-12-09 21:54:31 -0500

    ADOdb to 5.20.9 for PHP 7.1 fixes (CO-1569)

--- app/Vendor/adodb5/adodb-xmlschema03.inc.php
+++ app/Vendor/adodb5/adodb-xmlschema03.inc.php
@@ -175,7 +175,6 @@ class dbObject {
 	* Destroys the object
 	*/
 	function destroy() {
-		unset( $this );
 	}
 
 	/**
@@ -290,13 +289,14 @@ class dbTable extends dbObject {
 		switch( $this->currentElement ) {
 			case 'INDEX':
 				if( !isset( $attributes['PLATFORM'] ) OR $this->supportedPlatform( $attributes['PLATFORM'] ) ) {
-					$r = $this->addIndex( $attributes );
-					xml_set_object( $parser, $r );
+					$index = $this->addIndex( $attributes );
+					xml_set_object( $parser,  $index );
 				}
 				break;
 			case 'DATA':
 				if( !isset( $attributes['PLATFORM'] ) OR $this->supportedPlatform( $attributes['PLATFORM'] ) ) {
-					xml_set_object( $parser, $this->addData( $attributes ) );
+					$data = $this->addData( $attributes );
+					xml_set_object( $parser, $data );
 				}
 				break;
 			case 'DROP':
@@ -2379,7 +2379,6 @@ class adoSchema {
 	function Destroy() {
 		ini_set("magic_quotes_runtime", $this->mgq );
 		#set_magic_quotes_runtime( $this->mgq );
-		unset( $this );
 	}
 }
 
