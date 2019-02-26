b76c810865defc846e933a0bed09b7add811695f
Benn Oshrin <boshrin@users.noreply.github.com>
2019-02-26 09:58:53 -0500

    Bump version to 3.2.1

--- app/Console/Command/UpgradeVersionShell.php
+++ app/Console/Command/UpgradeVersionShell.php
@@ -75,7 +75,8 @@ class UpgradeVersionShell extends AppShell {
     "3.0.0" => array('block' => false),
     "3.1.0" => array('block' => false, 'post' => 'post310'),
     "3.1.1" => array('block' => false),
-    "3.2.0" => array('block' => false)
+    "3.2.0" => array('block' => false),
+    "3.2.1" => array('block' => false)
   );
   
   public function getOptionParser() {
