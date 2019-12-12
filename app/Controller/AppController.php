89fcdf595ed6fec171e81a546725f01017df6543
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2019-12-12 15:53:32 +0200

    Apply Contain logic and filter out the un-necessary entries

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -1227,6 +1227,7 @@ class AppController extends Controller {
       $args['conditions']['CoOrgIdentityLink.co_person_id'] = $userId;
       $args['conditions']['Identifier.status'] = StatusEnum::Active;
       $args['conditions']['Identifier.login'] = true;
+      $args['contain']['CoOrgIdentityLink']['OrgIdentity'][] = 'Identifier';
 
       $userOrgIDs = $this->CoOrgIdentityLink->OrgIdentity->find('all', $args);
 
