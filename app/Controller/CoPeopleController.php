3231b327c50cb9776d171660536c55c59093d4b6
Jeanesily <5133114+Jeanesily@users.noreply.github.com>
2020-07-13 10:15:32 -0600

    Add comment to Group by

--- app/Controller/CoPeopleController.php
+++ app/Controller/CoPeopleController.php
@@ -37,6 +37,7 @@ class CoPeopleController extends StandardController {
   
   public $paginate = array(
     'limit' => 25,
+    // CO-1549. Group By CoPerson.id ensures duplicate users are not returned after joining with other tables (such as cm_names).
     'group' => array(
       'CoPerson.id', 'Co.id', 'PrimaryName.id'
     ),
