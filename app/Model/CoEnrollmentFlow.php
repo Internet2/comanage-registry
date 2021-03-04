ee16b9171ef0cb7f3f2622688809d9f2f98cf416
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2021-03-04 17:37:50 +0200

    Enrollment Flow Verified Mode.Verify CO Person Email if verified in OrgIdentity.

--- app/Model/CoEnrollmentFlow.php
+++ app/Model/CoEnrollmentFlow.php
@@ -183,7 +183,8 @@ class CoEnrollmentFlow extends AppModel {
       'rule' => array('inList',
                       array(VerificationModeEnum::Automatic,
                             VerificationModeEnum::None,
-                            VerificationModeEnum::Review)),
+                            VerificationModeEnum::Review,
+                            VerificationModeEnum::SelfReview)),
       'required' => false,
       'allowEmpty' => true
     ),
