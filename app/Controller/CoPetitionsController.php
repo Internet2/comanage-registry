afbac96990875cf88486055ceb901350e871c233
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2021-05-02 12:10:04 +0300

    Base64 en(de)code the identifier as part of the url

--- app/Controller/CoPetitionsController.php
+++ app/Controller/CoPetitionsController.php
@@ -1012,18 +1012,8 @@ class CoPetitionsController extends StandardController {
           // Because the URL is in a parameter, we expect it to be encoded.
           // We use base64 to avoid weird parsing errors with partially
           // visible URLs in a URL.
-          
-          // base64 encoding can generate some HTML special characters.
-          // We could urlencode, but that creates various confusion with different
-          // parts of the web transaction possibly urldecoding prematurely, so
-          // instead we substitute the problematic characters with others. See
-          // discussion in CO-1667 and https://stackoverflow.com/questions/1374753/passing-base64-encoded-strings-in-url
-          $returnUrl = base64_decode(str_replace(array(".", "_", "-"),
-                                                 // This mapping is the same as the one used by the YUI library.
-                                                 // RFC 4648 base64url is another option, but strangely doesn't
-                                                 // map the padding character (=).
-                                                 array("+", "/", "="),
-                                                 $this->request->params['named']['return']));
+
+          $returnUrl = cmg_urldecode($this->request->params['named']['return']);
         }
         
         $ptid = $this->CoPetition->initialize($efId,
