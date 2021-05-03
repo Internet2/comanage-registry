ad8ff2eb6b14530c272ce8e222631a4f1ee3b449
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2021-05-03 18:38:34 +0300

    Add filter banner to CO Job index view

--- app/Controller/CoJobsController.php
+++ app/Controller/CoJobsController.php
@@ -53,15 +53,106 @@ class CoJobsController extends StandardController {
    */
 
   function beforeRender() {
-    parent::beforeRender();
-    
-    if($this->action == 'view') {
-      if(in_array($this->viewVars['co_jobs'][0]['CoJob']['status'], array(JobStatusEnum::InProgress, JobStatusEnum::Queued))) {
+
+    global $cm_lang, $cm_texts;
+    if(!$this->request->is('restful')) {
+      if($this->action == 'index') {
+        $vv_job_type = array();
+        foreach($this->Co->loadAvailablePlugins('job') as $jPlugin) {
+          $job_models_keys = $job_models_values = array();
+          $pluginModel = ClassRegistry::init($jPlugin->name . "." . $jPlugin->name);
+          $plugin_name = $jPlugin->name;
+
+          $job_models = $pluginModel->getAvailableJobs();
+          $job_models_keys = array_map(
+            function($model) use ($plugin_name) {
+              return $plugin_name . "." . $model;
+            },
+            array_keys($job_models)
+          );
+          $job_models_values = array_map(
+            function($model) use ($plugin_name) {
+              return $model . " (" . $plugin_name . ")";
+            },
+            array_keys($job_models)
+          );
+          $job_type = array_combine($job_models_keys, $job_models_values);
+          $vv_job_type = array_merge($vv_job_type, $job_type);
+        }
+        $this->set('vv_statuses', $cm_texts[$cm_lang]['en.status.job']);
+        $this->set('vv_job_type', $vv_job_type);
+      } elseif($this->action == 'view'
+               && in_array($this->viewVars['co_jobs'][0]['CoJob']['status'], array(JobStatusEnum::InProgress, JobStatusEnum::Queued))) {
         // Request the page auto-refresh
-        
+
         $this->set('vv_refresh_interval', 15);
       }
     }
+    parent::beforeRender();
+  }
+
+  /**
+   * Determine the conditions for pagination of the index view, when rendered via the UI.
+   *
+   * @since  COmanage Registry v4.0.0
+   * @return Array An array suitable for use in $this->paginate
+   */
+
+  public function paginationConditions() {
+    // Job Type
+    $job_type = "";
+    if(isset($this->request->params['named']['search.jobType'])) {
+      // Undo Dot replace
+      $this->request->params['named']['search.jobType'] = str_replace(array("+", "/", "="),
+                                                                      array(".", "_", "-"),
+                                                                      $this->request->params['named']['search.jobType']);
+      $job_type = $this->request->params['named']['search.jobType'];
+    }
+    // Job Status
+    $job_status = isset($this->request->params['named']['search.status']) ? $this->request->params['named']['search.status'] : "";
+
+    $ret = array();
+    $ret['conditions'] = array();
+    $ret['conditions']['CoJob.co_id'] = $this->cur_co['Co']['id'];
+    if(!empty($job_status)) {
+      $ret['conditions']['CoJob.status'] = $job_status;
+    }
+    if(!empty($job_type)) {
+      $ret['conditions']['CoJob.job_type LIKE'] = "%{$job_type}%";
+    }
+    if(isset($this->view_contains)) {
+      $ret['contain'] = $this->view_contains;
+    }
+
+    return $ret;
+  }
+
+  /**
+   * Insert search parameters into URL for index.
+   * - postcondition: Redirect generated
+   *
+   * @since  COmanage Registry v4.0.0
+   */
+
+  public function search() {
+    $url['action'] = 'index';
+    $url['co'] = $this->cur_co['Co']['id'];
+
+    // build a URL will all the search elements in it
+    // the resulting URL will be similar to example.com/registry/co_groups/index/co:2/search.status:S
+    foreach($this->data['search'] as $field=>$value){
+      if(!empty($value)) {
+        if($field == "jobType") {
+          $value = str_replace(array(".", "_", "-"),
+                               array("+", "/", "="),
+                               $value);
+        }
+        $url['search.'.$field] = $value;
+      }
+    }
+
+    // redirect the user to the url
+    $this->redirect($url, null, true);
   }
   
   /**
@@ -110,6 +201,7 @@ class CoJobsController extends StandardController {
     
     // View all CO Jobs?
     $p['index'] = ($roles['cmadmin'] || $roles['coadmin']);
+    $p['search'] = $p['index'];
     
     // View this CO Job?
     $p['view'] = ($roles['cmadmin'] || $roles['coadmin']);
