17670e8795d994369b16033b79850abc53ae71d7
Benn Oshrin <boshrin@users.noreply.github.com>
2021-06-15 15:54:41 -0400

    Bump version to 3.3.4

--- app/Console/Command/UpgradeVersionShell.php
+++ app/Console/Command/UpgradeVersionShell.php
@@ -90,7 +90,8 @@ class UpgradeVersionShell extends AppShell {
     "3.3.0" => array('block' => false, 'pre' => 'pre330', 'post' => 'post330'),
     "3.3.1" => array('block' => false),
     "3.3.2" => array('block' => false),
-    "3.3.3" => array('block' => false)
+    "3.3.3" => array('block' => false),
+    "3.3.4" => array('block' => false)
   );
   
   public function getOptionParser() {
