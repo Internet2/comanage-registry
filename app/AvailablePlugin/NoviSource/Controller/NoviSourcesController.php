87c22e352ae212d3b5734599dbeee71ada15ce6c
Benn Oshrin <boshrin@users.noreply.github.com>
2021-10-16 16:47:04 -0400

    Initial implementation of NoviSource (CO-2247)

---
+++ app/AvailablePlugin/NoviSource/Controller/NoviSourcesController.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * COmanage Registry Novi Source Controller
+ *
+ * Portions licensed to the University Corporation for Advanced Internet
+ * Development, Inc. ("UCAID") under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * UCAID licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @link          https://www.internet2.edu/comanage COmanage Project
+ * @package       registry
+ * @since         COmanage Registry v4.1.0
+ * @license       Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ */
+
+App::uses("SOISController", "Controller");
+
+class NoviSourcesController extends SOISController {
+  // Class name, used by Cake
+  public $name = "NoviSources";
+  
+  /**
+   * Authorization for this Controller, called by Auth component
+   * - precondition: Session.Auth holds data used for authz decisions
+   * - postcondition: $permissions set with calculated permissions
+   *
+   * @since  COmanage Registry v4.1.0
+   * @return Array Permissions
+   */
+  
+  function isAuthorized() {
+    $roles = $this->Role->calculateCMRoles();
+    
+    // Construct the permission set for this user, which will also be passed to the view.
+    $p = array();
+    
+    // Determine what operations this user can perform
+    
+    // Delete an existing NoviSource?
+    $p['delete'] = ($roles['cmadmin'] || $roles['coadmin']);
+    
+    // Edit an existing NoviSource?
+    $p['edit'] = ($roles['cmadmin'] || $roles['coadmin']);
+    
+    // View all existing NoviSource?
+    $p['index'] = ($roles['cmadmin'] || $roles['coadmin']);
+    
+    // View an existing NoviSource?
+    $p['view'] = ($roles['cmadmin'] || $roles['coadmin']);
+    
+    $this->set('permissions', $p);
+    return($p[$this->action]);
+  }
+}
