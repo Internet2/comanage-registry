3521624ebd7a8a467c30e1ba0b851846a4fa9a18
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2023-06-07 20:11:24 +0300

    CO-2403_Prevent_Enrollment_flow_for_authenticated_user (#487)
    * Duplicate Account Check Enroller Plugin
    
    * Configuration page
    
    * Basic duplicate identifier check
    
    * Search for duplicates both under the CoPerson and OrgIdentity
    
    * Rename to DuplicateCheckEnroller
    
    * Make edit file a symbolic link

---
+++ app/AvailablePlugin/DuplicateCheckEnroller/Controller/DuplicateCheckEnrollerCoPetitionsController.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * COmanage RegistryDuplicate Check Enroller Plugin CoPetitions Controller
+ *
+ * Portions licensed to the University Corporation for Advanced Internet
+ * Development, Inc. ("UCAID") under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * UCAID licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @link          http://www.internet2.edu/comanage COmanage Project
+ * @package       registry-plugin
+ * @since         COmanage Registry v4.3.0
+ * @license       Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ */
+
+App::uses('CoPetitionsController', 'Controller');
+
+class DuplicateCheckEnrollerCoPetitionsController extends CoPetitionsController {
+  // Class name, used by Cake
+  public $name = "DuplicateCheckEnrollerCoPetitionsController";
+
+  public $uses = array(
+    "CoPetition",
+    "DuplicateCheckEnroller.DuplicateCheckEnroller"
+  );
+
+
+  /**
+   * Plugin functionality following finalize step
+   *
+   * @param   Integer  $id        CO Petition ID
+   * @param   Array    $onFinish  URL, in Cake format
+   */
+
+  protected function execute_plugin_finalize($id, $onFinish) {
+    // Finished the updates. Return to the petition
+    $this->redirect($onFinish);
+  }
+
+  /**
+   * Plugin functionality following Start step
+   *
+   * @param   Integer  $id        CO Petition ID
+   * @param   Array    $onFinish  URL, in Cake format
+   */
+
+  protected function execute_plugin_start($id, $onFinish) {
+    $args = array();
+    $args['conditions']['DuplicateCheckEnroller.co_enrollment_flow_wedge_id'] = $this->viewVars['vv_efwid'];
+    $args['contain'] = false;
+
+    $duplicate_account = $this->DuplicateCheckEnroller->find('first', $args);
+    if(empty($duplicate_account)) {
+      throw new RuntimeException(_txt('er.duplicate_check_enrollers.cfg.notfound'));
+    }
+
+
+    $this->set('vv_duplicate_account', $duplicate_account);
+    $this->set('vv_petition_id', $id);
+
+    $remote_user = getenv($duplicate_account['DuplicateCheckEnroller']['env_remote_user']);
+
+    if(empty($remote_user)) {
+      throw new RuntimeException(_txt('er.duplicate_check_enrollers.remote_user.notfound'));
+    }
+
+    $ident_type = $duplicate_account['DuplicateCheckEnroller']['identifier_type'];
+
+    // CO Person linked to identifier through CO Person
+    $co_person = $this->DuplicateCheckEnroller->searchCoPersonDuplicate($this->cur_co["Co"]["id"], $remote_user, $ident_type);
+    // CO Person linked to identifier through CO Person
+    $co_person_via_org = $this->DuplicateCheckEnroller->findOrgIdentityDuplicate($this->cur_co["Co"]["id"], $remote_user, $ident_type);
+
+    if(!empty($co_person)
+       || !empty($co_person_via_org)) {
+      // Redirect according to the configuration
+      if(!empty($duplicate_account['DuplicateCheckEnroller']['redirect_url'])) {
+        $this->redirect($duplicate_account['DuplicateCheckEnroller']['redirect_url']);
+      }
+
+      $this->Flash->set(_txt('er.ia.exists', array($remote_user)), array('key' => 'error'));
+
+      $co_person_id = $co_person["CoPerson"]["id"] ?? $co_person_via_org["CoPerson"]["id"] ?? null;
+      if(is_null($co_person_id) && !empty($co_person_via_org["OrgIdentity"]["id"])) {
+        // The identifier exists but is not linked to a CO Person
+        // We will redirect to the root
+        $this->redirect("/");
+      }
+
+      $this->redirect(array('plugin' => null,
+                            'controller' => 'co_people',
+                            'action'     => 'canvas',
+                             $co_person_id));
+    }
+
+    $this->redirect($onFinish);
+  }
+}
---
+++ app/AvailablePlugin/DuplicateCheckEnroller/Model/DuplicateCheckEnroller.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * COmanage Registry Duplicate Check Enroller Plugin Model
+ *
+ * Portions licensed to the University Corporation for Advanced Internet
+ * Development, Inc. ("UCAID") under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * UCAID licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @link          http://www.internet2.edu/comanage COmanage Project
+ * @package       registry-plugin
+ * @since         COmanage Registry v4.3.0
+ * @license       Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ */
+
+class DuplicateCheckEnroller extends AppModel {
+  // Required by COmanage Plugins
+  public $cmPluginType = "enroller";
+
+  // Document foreign keys
+  public $cmPluginHasMany = array(
+    "CoEnrollmentFlowWedge" => "DuplicateCheckEnroller"
+  );
+
+  // Add behaviors
+  public $actsAs = array('Containable', 'Changelog' => array('priority' => 5));
+
+  // Association rules from this model to other models
+  public $belongsTo = array("CoEnrollmentFlowWedge");
+
+  public $hasMany = array();
+
+  // Default display field for cake generated views
+  public $displayField = "co_enrollment_flow_wedge_id";
+
+  // Validation rules for table elements
+  public $validate = array(
+    'co_enrollment_flow_wedge_id' => array(
+      'rule' => 'numeric',
+      'required' => true,
+      'allowEmpty' => false
+    ),
+    'env_remote_user' => array(
+      'rule' => '/.*/',
+      'required' => true,
+      'allowEmpty' => false
+    ),
+    'identifier_type' => array(
+      'content' => array(
+        'rule' => array('validateExtendedType',
+          array('attribute' => 'Identifier.type',
+                'default' => array(IdentifierEnum::AffiliateSOR,
+                                   IdentifierEnum::Badge,
+                                   IdentifierEnum::Enterprise,
+                                   IdentifierEnum::ePPN,
+                                   IdentifierEnum::ePTID,
+                                   IdentifierEnum::ePUID,
+                                   IdentifierEnum::GuestSOR,
+                                   IdentifierEnum::HRSOR,
+                                   IdentifierEnum::Mail,
+                                   IdentifierEnum::National,
+                                   IdentifierEnum::Network,
+                                   IdentifierEnum::OIDCsub,
+                                   IdentifierEnum::OpenID,
+                                   IdentifierEnum::ORCID,
+                                   IdentifierEnum::ProvisioningTarget,
+                                   IdentifierEnum::Reference,
+                                   IdentifierEnum::SamlPairwise,
+                                   IdentifierEnum::SamlSubject,
+                                   IdentifierEnum::SORID,
+                                   IdentifierEnum::StudentSOR,
+                                   IdentifierEnum::UID))),
+        'required' => true,
+        'allowEmpty' => false
+      )
+    ),
+    'redirect_url' => array(
+      'rule' => '/.*/',
+      'required' => false,
+      'allowEmpty' => true
+    )
+  );
+
+  /**
+   * Check for CO Person Identifier duplicates.
+   *
+   * @since  COmanage Registry v4.3.0
+   * @param  integer $coId                    CO ID
+   * @param  string  $env_remote_user         Identifier from Env
+   * @param  string  $ident_type              Identifier Type
+   * @return Array
+   */
+
+  public function searchCoPersonDuplicate($coId, $env_remote_user, $ident_type) {
+    $args = array();
+    $args['joins'][0]['table'] = 'co_people';
+    $args['joins'][0]['alias'] = 'CoPerson';
+    $args['joins'][0]['type'] = 'INNER';
+    $args['joins'][0]['conditions'][0] = 'Identifier.co_person_id=CoPerson.id';
+    $args['conditions']['Identifier.identifier'] = $env_remote_user;
+    $args['conditions']['Identifier.status'] = SuspendableStatusEnum::Active;
+    $args['conditions']['Identifier.type'] = $ident_type;
+    $args['conditions']['CoPerson.co_id'] = $coId;
+    $args['conditions']['CoPerson.status'] = array(StatusEnum::Active, StatusEnum::GracePeriod);
+    $args['fields'] = array('CoPerson.id');
+    $args['contain'] = false;
+
+    $Identifier = ClassRegistry::init('Identifier');
+    return $Identifier->find('first', $args);
+  }
+
+
+  /**
+   * Check for CO Person Identifier duplicates through OrgIdentity.
+   *
+   * @since  COmanage Registry v4.3.0
+   * @param  integer $coId                    CO ID
+   * @param  string  $env_remote_user         Identifier from Env
+   * @param  string  $ident_type              Identifier Type
+   * @return Array
+   */
+
+  public function findOrgIdentityDuplicate($coId, $env_remote_user, $ident_type) {
+    $args = array();
+    $args['joins'][0]['table'] = 'org_identities';
+    $args['joins'][0]['alias'] = 'OrgIdentity';
+    $args['joins'][0]['type'] = 'INNER';
+    $args['joins'][0]['conditions'][0] = 'Identifier.org_identity_id=OrgIdentity.id';
+    $args['joins'][1]['table'] = 'co_org_identity_links';
+    $args['joins'][1]['alias'] = 'CoOrgIdentityLink';
+    $args['joins'][1]['type'] = 'INNER';
+    $args['joins'][1]['conditions'][0] = 'OrgIdentity.id=CoOrgIdentityLink.org_identity_id';
+    $args['joins'][2]['table'] = 'co_people';
+    $args['joins'][2]['alias'] = 'CoPerson';
+    $args['joins'][2]['type'] = 'INNER';
+    $args['joins'][2]['conditions'][0] = 'CoPerson.id=CoOrgIdentityLink.co_person_id';
+    $args['conditions']['Identifier.identifier'] = $env_remote_user;
+    $args['conditions']['Identifier.status'] = SuspendableStatusEnum::Active;
+    $args['conditions']['Identifier.type'] = $ident_type;
+    $args['conditions']['OrgIdentity.co_id'] = $coId;
+    $args['conditions']['CoPerson.status'] = array(StatusEnum::Active, StatusEnum::GracePeriod);
+    $args['fields'] = array('CoPerson.id', 'OrgIdentity.id', 'Identifier.id');
+    $args['contain'] = false;
+
+    $Identifier = ClassRegistry::init('Identifier');
+    return $Identifier->find('first', $args);
+  }
+
+  /**
+   * Expose menu items.
+   *
+   * @since COmanage Registry v4.3.0
+   * @return Array with menu location type as key and array of labels, controllers, actions as values.
+   */
+
+  public function cmPluginMenus() {
+    return array();
+  }
+}
