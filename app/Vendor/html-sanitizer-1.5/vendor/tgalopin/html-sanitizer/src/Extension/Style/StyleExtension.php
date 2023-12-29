019749e6beea87585c9aa981bfdef42349643413
Arlen Johnson <arlen@sphericalcowgroup.com>
2023-12-29 13:53:49 -0500

    Commit the custom style extension for html-sanitizer (CO-2719)

---
+++ app/Vendor/html-sanitizer-1.5/vendor/tgalopin/html-sanitizer/src/Extension/Style/StyleExtension.php
@@ -0,0 +1,42 @@
+<?php
+  /**
+   * COmanage Style Node Extension to HTML sanitizer
+   *
+   * Portions licensed to the University Corporation for Advanced Internet
+   * Development, Inc. ("UCAID") under one or more contributor license agreements.
+   * See the NOTICE file distributed with this work for additional information
+   * regarding copyright ownership.
+   *
+   * UCAID licenses this file to you under the Apache License, Version 2.0
+   * (the "License"); you may not use this file except in compliance with the
+   * License. You may obtain a copy of the License at:
+   *
+   * http://www.apache.org/licenses/LICENSE-2.0
+   *
+   * Unless required by applicable law or agreed to in writing, software
+   * distributed under the License is distributed on an "AS IS" BASIS,
+   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   * See the License for the specific language governing permissions and
+   * limitations under the License.
+   *
+   * @link          http://www.internet2.edu/comanage COmanage Project
+   * @link          http://www.internet2.edu/comanage COmanage Project
+   * @package       registry
+   * @since         COmanage Registry v4.3.3
+   * @license       Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+   */
+namespace HtmlSanitizer\Extension\Style;
+
+use HtmlSanitizer\Extension\ExtensionInterface;
+
+class StyleExtension implements ExtensionInterface {
+  public function getName(): string {
+    return 'style';
+  }
+
+  public function createNodeVisitors(array $config = []): array {
+    return [
+      'style' => new NodeVisitor\StyleNodeVisitor($config['tags']['style'] ?? []),
+    ];
+  }
+}
