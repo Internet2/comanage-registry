24c63c071cd19870e4801d0656a37fa8e73cfbde
Shayna Atkinson <satkinson@sphericalcowgroup.com>
2024-04-30 09:43:43 -0400

    bump version to 4.3.3

--- app/Console/Command/UpgradeVersionShell.php
+++ app/Console/Command/UpgradeVersionShell.php
@@ -106,7 +106,8 @@ class UpgradeVersionShell extends AppShell {
     "4.2.1" => array('block' => false),
     "4.3.0" => array('block' => false, 'post' => 'post430'),
     "4.3.1" => array('block' => false),
-    "4.3.2" => array('block' => false)
+    "4.3.2" => array('block' => false),
+    "4.3.3" => array('block' => false)
   );
   
   public function getOptionParser() {
