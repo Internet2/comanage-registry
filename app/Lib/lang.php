3f561e4c615a228b8b8144d68196878c722dd276
Arlen Johnson <arlen@dogenmedia.com>
2024-09-06 14:55:24 -0400

    Provide confirmation for petition Approve or Deny (CO-2815) (#636)
    * Add configuration for 'Require JavaScript Confirmation' and 'Require Comment' on "Approve" or "Deny" for petitions (CO-2815)
    
    * Implement front-end for 'Require JavaScript Confirmation' and 'Require Comment' on "Approve" or "Deny" for petitions (CO-2815)
    
    * Fix spinner and ensure correct behavior on "Approve" or "Deny" for petitions (CO-2815)
    
    * Improve conditional JavaScript for Enrollment Flow view and Petition Attributes (CO-2815)
    
    ---------
    
    Co-authored-by: Arlen Johnson <arlen@sphericalcowgroup.com>

--- app/Lib/lang.php
+++ app/Lib/lang.php
@@ -610,6 +610,20 @@ The request may be viewed at
     EnrollmentAuthzEnum::None           => 'None'
   ),
   
+  'en.enrollment.confmode' => array(
+    EnrollmentApprovalConfirmationModeEnum::Always   => 'Always',
+    EnrollmentApprovalConfirmationModeEnum::Approval => 'Approval Only',
+    EnrollmentApprovalConfirmationModeEnum::Denial   => 'Denial Only',
+    EnrollmentApprovalConfirmationModeEnum::Never    => 'Never',
+  ),
+  
+  'en.enrollment.confmode.placeholder' => array(
+    EnrollmentApprovalConfirmationModeEnum::Always   => 'Required',
+    EnrollmentApprovalConfirmationModeEnum::Approval => 'Required on approval',
+    EnrollmentApprovalConfirmationModeEnum::Denial   => 'Required on denial',
+    EnrollmentApprovalConfirmationModeEnum::Never    => 'Optional',
+  ),
+  
   'en.enrollment.dupe' => array(
     EnrollmentDupeModeEnum::Duplicate       => 'Flag as Duplicate',
     EnrollmentDupeModeEnum::Merge           => 'Merge',
@@ -1497,7 +1511,15 @@ The request may be viewed at
   'fd.ef.app.base' => 'App Base location',
   'fd.ef.app.base.desc' => 'Application\'s webroot location, e.g. /registry',
   'fd.ef.appr' =>     'Require Approval For Enrollment',
+  'fd.ef.appr.approve.confirm' => 'Are you sure you want to approve this petition?',
+  'fd.ef.appr.deny.confirm' => 'Are you sure you want to deny this petition?',
   'fd.ef.appr.desc' => 'If administrator approval is required, a Petition must be approved before the Enrollee becomes active.',
+  'fd.ef.appr.confmode' => 'Approval Confirmation Mode',
+  'fd.ef.appr.confmode.desc' => 'When to require JavaScript confirmation on "Approve" or "Deny" petition buttons',
+  'fd.ef.appr.invalid.appr' => 'You must supply a comment when approving a petition.',
+  'fd.ef.appr.invalid.deny' => 'You must supply a comment when denying a petition.',
+  'fd.ef.appr.reqcomment' => 'Require Comment on Approval',
+  'fd.ef.appr.reqcomment.desc' => 'When to require a comment to "Approve" or "Deny" a petition',
   'fd.ef.appr.pl' =>  'Approvers',
   'fd.ef.appr.gr' =>  'Members of this Group are authorized approvers (or else CO/COU admins by default)',
   'fd.ef.appr.ntf' =>  'Approver Notification Email Message Template',
